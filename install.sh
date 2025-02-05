9823f9155fcb177073a99e975258b1ea7d1e7695
dan <idkmaybeyoudo@gmail.com>
2025-02-05 22:32:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,25 @@ else
   echo "Installation failed. Please try again."
   exit 1
 fi
+}
+ 
+uninstall_ditxh() {
+     
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run this script with sudo or as root."
+        exit 1
+    fi
+ 
+    if [ -f "$INSTALL_DIR/$BINARY_NAME" ]; then
+        rm "$INSTALL_DIR/$BINARY_NAME"
+        echo "$BINARY_NAME uninstalled successfully!"
+    else
+        echo "$BINARY_NAME is not installed."
+    fi
+}
+ 
+if [ "$1" == "uninstall" ]; then
+    uninstall_ditxh
+else
+    install_ditxh
+fi
