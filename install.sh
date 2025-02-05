4cd9a608d4b2050eb9932b1a237860451cc6ccc6
dan <idkmaybeyoudo@gmail.com>
2025-02-05 22:44:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
- 
-BINARY_URL="https://github.com/danbyte123/dixth.git"
-
- 
+BINARY_URL="https://github.com/danbyte123/dixth/releases/latest/download/ditxh"
 INSTALL_DIR="/usr/local/bin"
+BINARY_NAME="ditxh"
 
- 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script with sudo or as root."
-  exit 1
-fi
-
- 
-echo "Downloading ditxh..."
-curl -L "$BINARY_URL" -o "$INSTALL_DIR/ditxh"
-
- 
-chmod +x "$INSTALL_DIR/ditxh"
-
- 
-if [ -f "$INSTALL_DIR/ditxh" ]; then
-  echo "ditxh installed successfully!"
-  echo "You can now run 'ditxh' from anywhere."
-else
-  echo "Installation failed. Please try again."
-  exit 1
-fi
-}
- 
-uninstall_ditxh() {
-     
+install_ditxh() {
     if [ "$EUID" -ne 0 ]; then
         echo "Please run this script with sudo or as root."
         exit 1
     fi
- 
+
+    echo "Downloading ditxh..."
+    curl -L "$BINARY_URL" -o "$INSTALL_DIR/$BINARY_NAME"
+
+    chmod +x "$INSTALL_DIR/$BINARY_NAME"
+
+    if [ -f "$INSTALL_DIR/$BINARY_NAME" ]; then
+        echo "ditxh installed successfully!"
+        echo "You can now run 'ditxh' from anywhere."
+    else
+        echo "Installation failed. Please try again."
+        exit 1
+    fi
+}
+
+uninstall_ditxh() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run this script with sudo or as root."
+        exit 1
+    fi
+
     if [ -f "$INSTALL_DIR/$BINARY_NAME" ]; then
         rm "$INSTALL_DIR/$BINARY_NAME"
         echo "$BINARY_NAME uninstalled successfully!"
@@ -43,7 +37,7 @@ uninstall_ditxh() {
         echo "$BINARY_NAME is not installed."
     fi
 }
- 
+
 if [ "$1" == "uninstall" ]; then
     uninstall_ditxh
 else
